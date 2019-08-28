3b861131dc28276981c8f2287ee4d3ff978560c1
Jungdo Lee <lee.jungdo@gmail.com>
2019-08-28 22:48:43 +0900

    Disable Turbolinks since it doesn't work well with react router

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from '../components/App'
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router>
       <Route path="/" component={App} />
